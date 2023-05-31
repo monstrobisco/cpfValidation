87442f100350c44a9b9f886917d7a1503fda8dea
thmedeiroslima <97640552+thmedeiroslima@users.noreply.github.com>
2023-05-31 11:11:54 -0300

    Inicial

---
+++ src/main/java/com/sicredi/cpfValidation/CpfValidationApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.cpfValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicredi/cpfValidation/CpfValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.cpfValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
